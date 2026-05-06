713b0ac0e6668313d7414f1eda786eabe55ec375
Alexandra Millatmal <alexandrajmh@gmail.com>
2026-05-06 17:36:56 -0400

    feat: continuing to build out popups

---
+++ data.js
@@ -0,0 +1,4 @@
+const DATA = {"New York": {
+    "1": {"width": "", "height": "", "top": "", "left": ""},
+    "7": {"width": "720", "height": "1080", "top": "", "left": ""},
+}}
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let LAT;
 let LON;
 let CITY;
-let FILE_COUNT = 33; // hard-coded for this iteration, would involve more advanced JS bundling to load dynamically.
+let FILE_COUNT = 8; // hard-coded for this iteration, would involve more advanced JS bundling to load dynamically.
 
 
 async function myOnload() {
@@ -22,20 +22,34 @@ async function myOnload() {
     }
 }
 
+// async function loadPage() {
+//     const currentPage = localStorage.getItem("count");
+//     let data;
+//     if (DATA[CITY][currentPage]) {
+//         data = DATA[CITY][currentPage];
+//         const el = document.getElementById("container")
+//         el.innerHTML=data;
+//     }
+// }
+
 async function onCascadeLoad() {
     const NEXT = parseInt(localStorage.getItem("count")) + 1;
     console.log("NEXT: ", NEXT);
     console.log("FILE_COUNT: ", FILE_COUNT);
     if (NEXT <= FILE_COUNT) {
         localStorage.setItem("count", NEXT.toString());
-        setTimeout(() => openNextPage(NEXT), 1000)
+        setTimeout(() => openNextPage(NEXT), 3000)
         //setTimeout(() => {console.log('timeout fired!')}, 5000);
     }
 }
 
 async function openNextPage(pageCount) {
-    
-    window.open(`./${pageCount}.html`, `Window${pageCount}`, config=`width=500, height=500`) // TODO open at a randomized location on screen
+    // TODO dynamically set width, height and maybe top, left values based off of data
+    console.log("in openNextPage: ", localStorage.getItem("CITY"))
+    const data = DATA[localStorage.getItem("CITY")][pageCount] || {};
+    const width = data["width"] || "500";
+    const height = data["height"] || "500";
+    window.open(`./${pageCount}.html`, `Window${pageCount}`, config=`width=${width}, height=${height}`) // TODO open at a randomized location on screen
 }
 
 async function setLocation(lat, lon) {
@@ -63,9 +77,15 @@ async function startCascade() {
     
 
     // get data, using city name from local storage
+    if (DATA[CITY]) {
+        window.open(`./popups/start.html`, "_unfencedTop", "width=500, height=500, left=200, top=200");
+    } else {
+        el = document.getElementById("container")
+        el.innerHTML = "<p>no data yet for your current location :(</p>"
+    }
     // if no data for city 
         // load a message about no info yet
     // if data
         // start cascade
-    window.open(`./popups/start.html`, "_unfencedTop", "width=500, height=500, left=200, top=200");
+   
 }
