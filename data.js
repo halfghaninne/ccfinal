291cf677d4fa42699f37593ad888a3781c61f7cd
Alexandra Millatmal <alexandrajmh@gmail.com>
2026-05-10 17:39:45 -0400

    chore: removes code comments

--- data.js
+++ data.js
@@ -5,34 +5,31 @@ const DATA = {"New York": {
     "3": {"width": "300", "height": "322", "loadInMs": 1000, "citation": "Portrait of George Stonefish by NMAI."},
     "4": {"url": "https://youtube.com/watch?v=9Cqe7JfP8uA?t=11&mode=theatre&mute=1", "width": "900", "loadInMs": 1000},
     "5": {"width": "480", "height": "720", "loadInMs": 1000},
-    // break
     "6": {"url": "https://www.youtube.com/watch?v=rm1NOlZzxVg&t=40s&mode=theatre", "width": "700", "loadInMs": 1000},
     "7": {"width": "756", "height": "522", "loadInMs": 1000}, // pdf
     "8": {"width": "550", "loadInMs": 1000, "citation" : "SOURCE"},
     "9": {"width": "620", "height": "443", "loadInMs": 1000},
-    // break
     "10": {"width": "", "height": "", "top": "", "left": "", "loadInMs": 1000},
     "11": {"width": "1000", "height": "", "top": "", "left": "", "loadInMs": 1000},
     "12": {"width": "546", "height": "345", "top": "", "left": "", "loadInMs": 1000},
-    //break
     "13": {"width": "370", "height": "225", "top": "", "left": "", "loadInMs": 1000}, 
     "14": {"width": "640", "height": "360", "top": "", "left": "", "loadInMs": 1000},
     "15": {"width": "661", "height": "521", "top": "", "left": "", "loadInMs": 1000}, // pdf
     "16": {"width": "400", "height": "300", "top": "", "left": "", "loadInMs": 1000},
-    // break
     "17": {"width": "460", "height": "307", "top": "", "left": "", "loadInMs": 1000},
     "18": {"width": "612", "height": "675", "loadInMs": 1000}, // pdf
     "19": {"width": "504", "height": "672", "loadInMs": 1000},
     "20": {"url": "https://www.youtube.com/watch?v=zZDCJ36PYxk&t=6s", "width": "700", "loadInMs": 1000},
-    "21": {"width": "", "height": "", "top": "", "left": "", "loadInMs": 1000}, // live feed 
+    // // TODO TODO TODO // //
+    "21": {"width": "", "height": "", "top": "", "left": "", "loadInMs": 1000}, // TODO live feed 
+    // /// //////////// // //
     "22": {"width": "360", "loadInMs": 1000},
     "23": {"width": "500", "height": "300", "loadInMs": 1000},
     "24": {"width": "497", "height": "501", "top": "", "left": "", "loadInMs": 1000},
     "25": {"width": "319", "height": "239", "loadInMs": 1000},
     "26": {"url": "https://www.youtube.com/watch?v=w7ASUgIKV0g", "width": "700", "loadInMs": 1000},
-    // break
     "27": {"width": "366", "height": "207", "top": "", "left": "", "loadInMs": 1000},
-    "28": {"url": "https://www.youtube.com/watch?v=vQz80JjqJ9g", width: "400", loadInMs: 1000}, // summer and winter url change
+    "28": {"url": "https://www.youtube.com/watch?v=vQz80JjqJ9g", width: "400", loadInMs: 1000}, // TODO summer and winter url change
     "29": {"width": "550", "height": "370", "top": "", "left": "", "loadInMs": 1000},
     "30": {"url": "https://youtu.be/V6yvGLhMOJU?t=565", "width": "700", "loadInMs": 1000},
     "31": {"width": "230", "height": "153", "loadInMs": 1000},
