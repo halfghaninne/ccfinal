5bd21d12577245b31ab07685ed466819e81ab767
Alexandra Millatmal <alexandrajmh@gmail.com>
2026-05-10 23:00:01 -0400

    style: refine through page 32

--- data.js
+++ data.js
@@ -6,9 +6,9 @@ const DATA = {"New York": {
     "4": {"url": "https://youtube.com/watch?v=9Cqe7JfP8uA?t=11&mode=theatre&mute=1", "width": "900", "loadInMs": 3000},
     "5": {"width": "480", "height": "720", "loadInMs": 3000, "citation": "Photo courtesy of The Public History Project."},
     "6": {"url": "https://www.youtube.com/watch?v=rm1NOlZzxVg&t=40s&mode=theatre", "width": "700", "loadInMs": 4000},
-    "7": {"width": "756", "height": "522", "top": "0", "left": "0", "loadInMs": 2000}, // pdf
+    "7": {"width": "756", "height": "522", "top": "0", "loadInMs": 2000}, // pdf
     "8": {"width": "550", "loadInMs": 3000, "citation" : "Image 1: Coles, Robert R. 'Wampum Belt and Shells.' The Long Island Indian. Glen Cove, NY: Little Museum, 1954. Image 2: Bolton, Reginald P. 'MAP VIII, D.' Indian Paths in the Great Metropolis. New York: Museum of the American Indian, 1922. Brooklyn Collection, Brooklyn Public Library. Both accessed via the Brooklyn Public Library's Native Americans in Brooklyn Primary Source Packet."},
-    "9": {"width": "620", "height": "443", "left": "10", "loadInMs": 10000, "citation": "Bolton, Reginald P. 'A Circular Bark-Covered House.' Indian Life of Long Ago in the City of New York. Port Washington, NY: I.J. Friedman, 1971."},
+    "9": {"width": "620", "height": "443", "left": "10", "loadInMs": 7000, "citation": "Bolton, Reginald P. 'A Circular Bark-Covered House.' Indian Life of Long Ago in the City of New York. Port Washington, NY: I.J. Friedman, 1971."},
     "10": {"width": "550", "left": "10", "top": "0", "loadInMs": 2000, "citation": "Photo from Wikimedia Creative Commons, taken by Loominosity Z on Flickr."},
     "11": {"width": "600", "height": "300", "loadInMs": 4000, "citation": "Photo from 'The Origin Story of Manhattan's Nerve Center.' Business Insider, 2014. https://www.businessinsider.com/60-hudson-street-documentary-2014-6"},
     "12": {"width": "546", "height": "345", "loadInMs": 1000},
@@ -20,22 +20,23 @@ const DATA = {"New York": {
     "17": {"width": "460", "height": "307", "loadInMs": 2000, "citation": "Photo from Wikimedia Creative Commons, uploaded by user DeMecheleir."},
     "18": {"width": "612", "height": "675", "loadInMs": 1000}, // pdf
     "19": {"width": "504", "height": "672", "loadInMs": 1000},
-    "20": {"url": "https://www.youtube.com/watch?v=zZDCJ36PYxk&t=6s&mode=theatre", "width": "700", "loadInMs": 1000},
+    "20": {"url": "https://www.youtube.com/watch?v=zZDCJ36PYxk&t=6s&mode=theatre", "width": "700", "loadInMs": 1500},
     // // TODO TODO TODO // //
     "21": {"width": "", "height": "", "top": "", "left": "", "loadInMs": 2000}, // TODO live feed 
     // /// //////////// // //
-    "22": {"width": "360", "height": "320", "loadInMs": 2000},
-    "23": {"width": "500", "height": "300", "loadInMs": 1500},
-    "24": {"width": "497", "height": "501", "loadInMs": 1500, "citation": "Portrait of Derrick Ingram courtesy of the Surveillance Technology Oversight Project."}, // portrait
-    "25": {"width": "319", "height": "239", "loadInMs": 3000},
-    "26": {"url": "https://www.youtube.com/watch?v=w7ASUgIKV0g&mode=theatre", "width": "700", "loadInMs": 1000},
-    "27": {"width": "366", "height": "207", "top": "", "left": "", "loadInMs": 1000},
-    /// 
-    "28": {"url": "https://www.youtube.com/watch?v=vQz80JjqJ9g&mode=theatre", width: "400", loadInMs: 1000}, // TODO summer and winter url change
-    "29": {"width": "550", "height": "370", "top": "0", "left": "0", "loadInMs": 1000},
-    "30": {"url": "https://youtu.be/V6yvGLhMOJU?t=565&mode=theatre", "width": "700", "loadInMs": 1000},
-    "31": {"width": "230", "height": "153", "loadInMs": 1000},
-    "32": {"width": "550", "height": "429", "top": "370", "left": "0", "loadInMs": 1000}, // TODO align placement with #29
+    "22": {"width": "360", "height": "320", "loadInMs": 2000, "citation": "Photo from Wikimedia Creative Commons."},
+    "23": {"width": "500", "height": "300", "loadInMs": 2000, "citation": "Photo from Wikimedia Creative Commons."},
+    "24": {"width": "497", "height": "501", "loadInMs": 2000, "citation": "Portrait of Derrick Ingram courtesy of the Surveillance Technology Oversight Project."}, // portrait
+    "25": {"width": "500", "height": "374", "loadInMs": 3000},
+    "26": {"url": "https://www.youtube.com/watch?v=w7ASUgIKV0g&mode=theatre", "width": "700", "loadInMs": 2000},
+    "27": {"width": "600", "height": "339", "top": "", "left": "", "loadInMs": 1000}, //TODO; make bigger
+    //////
+    "28": {"url": "https://www.youtube.com/watch?v=vQz80JjqJ9g&mode=theatre", width: "700", loadInMs: 1000}, // TODO summer and winter url change
+    "29": {"width": "550", "height": "370", "top": "0", "left": "0", "loadInMs": 5000},
+    "30": {"url": "https://youtu.be/V6yvGLhMOJU?t=565&mode=theatre", "width": "700", "loadInMs": 2000},
+    "31": {"width": "420", "height": "280", "loadInMs": 1500},
+    "32": {"width": "550", "height": "429", "top": "450", "left": "0", "loadInMs": 2000}, 
+    ////
     "33": {"width": "409", "height": "325", "top": "", "left": "", "loadInMs": 1000},
     "34": {"width": "500", "height": "333", "top": "", "left": "", "loadInMs": 1000},
     "35": {"url": "https://www.youtube.com/watch?v=tDsS9ugo2TA&t=36s&mode=theatre", "width": "400", "loadInMs": 1000},
